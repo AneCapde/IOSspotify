5df8254a8b713118432b36440461dd10cb5ae469
DropMaster <michal.kru@wp.pl>
2021-11-10 17:11:25 +0100

    content view, and Info.plist fixed. New bug - EXC_BAD_ACCESS (code=EXC_I386_GPFLT)

--- IOSspotify/Views/ContentView.swift
+++ IOSspotify/Views/ContentView.swift
@@ -1,21 +1,38 @@
+
+//  Home.swift
+//  music_player_app
 //
-//  ContentView.swift
-//  IOSspotify
-//
-//  Created by Ane Capdevila on 10/11/2021.
+//  Created by Michalina Janik on 07/11/2021.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var model = ModelMusicPlayer.singletonMusicPlayer
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            PlaylistView().environmentObject(model)
+                .tabItem{
+                VStack {
+                    Image(systemName: "music.note.list")
+                    Text("Playlists")
+                }
+            }
+            LibraryView().environmentObject(model)
+                .tabItem{
+                VStack{
+                    Image(systemName: "music.note")
+                    Text("Library")
+                    
+                }
+            }
+            
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+       ContentView()
     }
 }
